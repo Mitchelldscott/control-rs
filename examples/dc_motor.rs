41b62398cc4926524ff733b97576ab9ea6c52fc0
Mitchell D Scott <scott.mitchell913@gmail.com>
2025-02-17 12:41:48 -0800

    update dc_motor example

--- examples/dc_motor.rs
+++ examples/dc_motor.rs
@@ -55,7 +55,6 @@ fn main() {
     // Denominator: [L, R + Kb]
     let motor_tf = TransferFunction::new([1.0], [l, r + kb]);
 
-    #[cfg(feature = "std")]
     println!("DC Motor {motor_tf}");
     println!("DC Gain: {:?}", dcgain(&motor_tf));
     println!("LHP: {:?}", lhp(&motor_tf));
